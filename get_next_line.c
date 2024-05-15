d79d013305fea6bdf1b112348538f2a57800a28a
Miguel Costa <miguelcosta@Miguels-Air.lan>
2024-05-15 20:25:18 +0100

    updated

--- get_next_line.c
+++ get_next_line.c
@@ -1,22 +1,55 @@
 #include "get_next_line.h"
 
+int ft_strlen(char *str)
+{
+    int i;
+    i = 0;
+
+    if (str == NULL)
+        return 0;
+    while(str[i])
+        i++;
+    return i;
+}
+
+char *ft_strjoin(char *s1, char *s2)
+{
+    char *str;
+    int i = 0;
+    int j = 0;
+
+
+    str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+    if(!str)
+        return NULL;
+    while(s1[i] && s1[i] != '\0')
+    {
+        str[i] = s1[i];
+        i++;
+    }
+    while(s2[j] && s2[j] != '\n')
+    {
+        str[i + j] = s2[j];
+        j++;
+    }
+    if (s2[j] == '\n')
+        str[i + j] = '\n';
+    str[i + j] = '\0';
+    return str;
+}
+
 char *get_next_line(int fd)
 {
     static char buffer[BUFFER_SIZE + 1];
     char *str;
-    int bytes;
 
-    bytes = 1;
     if(!buffer[0])
-        read(fd,buffer,BUFFER_SIZE);
-    str = NULL;
-    str = ft_strjoin(str,buffer);
-    while (bytes > 0 && find_line(buffer) == 0)
-    {
-        bytes = read(fd, buffer, BUFFER_SIZE);
-        str = ft_strjoin(str, buffer);
+    { 
+        read(fd, buffer, BUFFER_SIZE);
+        
     }
-    
+    str = NULL;
+    str = ft_strjoin(str, buffer);
     return str;
 }
 
